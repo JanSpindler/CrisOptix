0bdea96ffd8032b8548bfb65d1a76afa5e5eb921
JanSpindler <JanSpindler>
2024-05-03 13:57:46 +0200

    started to import DeviceBuffer from framework

--- include/DeviceBuffer.h
+++ include/DeviceBuffer.h
@@ -1,9 +1,23 @@
 #pragma once
 
+#include <cstddef>
+
 template <typename T>
 class DeviceBuffer
 {
 public:
+	constexpr DeviceBuffer(size_t count = 0)
+	{
+		Alloc(count);
+	}
+
+	constexpr ~DeviceBuffer()
+	{
+		Free();
+	}
 
 private:
+	size_t m_Count = 0;
+	size_t m_AllocCount = 0;
+	T* m_Ptr = nullptr;
 };
