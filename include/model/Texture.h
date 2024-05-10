9294d8ac993f8e825740ae2b0296e7a7ff17d9df
JanSpindler <JanSpindler>
2024-05-10 18:08:37 +0200

    upload image data onto device texture object

--- include/model/Texture.h
+++ include/model/Texture.h
@@ -1,11 +1,18 @@
 #pragma once
 
 #include <string>
+#include <stb_image.h>
+#include <cuda_runtime.h>
 
 class Texture
 {
 public:
 	Texture(std::string filePath);
-	
+	~Texture();
+
 private:
+	cudaArray_t m_DeviceData = nullptr;
+	cudaTextureObject_t m_DeviceTex = 0;
+
+	void LoadToDevice(const stbi_uc* imageData, const int width, const int height);
 };
--- src/Texture.cpp
+++ src/Texture.cpp
@@ -2,6 +2,8 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 #include <util/Log.h>
+#include <cuda_runtime.h>
+#include <util/custom_assert.h>
 
 Texture::Texture(std::string filePath)
 {
@@ -20,8 +22,49 @@ Texture::Texture(std::string filePath)
 	Log::Assert(data != nullptr, "STB failed to load image " + filePath);
 
 	// Load onto device
-	// TODO
+	LoadToDevice(data, width, height);
 
 	// Free host memory
 	stbi_image_free(data);
+}
+
+Texture::~Texture()
+{
+	ASSERT_CUDA(cudaDestroyTextureObject(m_DeviceTex));
+	ASSERT_CUDA(cudaFreeArray(m_DeviceData));
+}
+
+
+void Texture::LoadToDevice(const stbi_uc* imageData, const int width, const int height)
+{
+	const cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<uchar4>();
+	ASSERT_CUDA(cudaMallocArray(&m_DeviceData, &channelDesc, width, height));
+
+	const size_t pitch = width * 4 * sizeof(uint8_t);
+	ASSERT_CUDA(cudaMemcpy2DToArray(m_DeviceData, 0, 0, imageData, pitch, pitch, height, cudaMemcpyHostToDevice));
+
+	cudaResourceDesc resDesc{};
+	resDesc.resType = cudaResourceTypeArray;
+	resDesc.res.array.array = m_DeviceData;
+
+	cudaTextureDesc texDesc{};
+	texDesc.addressMode[0] = cudaAddressModeClamp;
+	texDesc.addressMode[1] = cudaAddressModeClamp;
+	texDesc.addressMode[2] = cudaAddressModeWrap;
+	texDesc.filterMode = cudaFilterModeLinear;
+	texDesc.readMode = cudaReadModeNormalizedFloat;
+	texDesc.sRGB = 0;
+	texDesc.borderColor[0] = 0.0f;
+	texDesc.borderColor[1] = 0.0f;
+	texDesc.borderColor[2] = 0.0f;
+	texDesc.borderColor[3] = 0.0f;
+	texDesc.normalizedCoords = 1;
+	texDesc.maxAnisotropy = 1;
+	texDesc.mipmapFilterMode = cudaFilterModePoint;
+	texDesc.mipmapLevelBias = 0.0f;
+	texDesc.minMipmapLevelClamp = 0;
+	texDesc.maxMipmapLevelClamp = 99;
+	texDesc.disableTrilinearOptimization = 0;
+	texDesc.seamlessCubemap = 0;
+	ASSERT_CUDA(cudaCreateTextureObject(&m_DeviceTex, &resDesc, &texDesc, nullptr));
 }
