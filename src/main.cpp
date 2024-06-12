a62543435ac22b56db3f3914c2c6121a7c178f39
JanSpindler <JanSpindler>
2024-06-12 13:10:01 +0200

    camera pos working

--- src/main.cpp
+++ src/main.cpp
@@ -94,7 +94,7 @@ int main()
 
     // Camera
     Camera cam(
-        glm::vec3(0.0f),
+        glm::vec3(0.0f, 4.0f, -15.0f),
         glm::vec3(0.0f, 0.0f, 1.0f),
         glm::vec3(0.0f, 1.0f, 0.0f),
         static_cast<float>(width) / static_cast<float>(height),
